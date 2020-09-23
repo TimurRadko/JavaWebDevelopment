d4f7105a03ba0b6e1b5e2a4434a5ec9f6cda4fe4
Timur Radko <timurradko@gmail.com>
2020-09-23 16:28:13 +0300

    add some changing to test class

--- ReverseArgsTask/src/test/java/ReverseArgsTest.java
+++ ReverseArgsTask/src/test/java/ReverseArgsTest.java
@@ -1,12 +1,9 @@
-import com.epam.reverse.args.data.DataReceiverFactory;
-import com.epam.reverse.args.data.DataReceivingFactoryImpl;
+import com.epam.reverse.args.data.receiver.type.ConsoleDataReceiver;
 import com.epam.reverse.args.data.receiver.type.ConsoleReceiver;
 import com.epam.reverse.args.logic.Calculator;
-import com.epam.reverse.args.view.PrinterFactory;
-import com.epam.reverse.args.view.PrinterFactoryImpl;
+import com.epam.reverse.args.view.type.ConsolePrinter;
 import com.epam.reverse.args.view.type.Printer;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -17,24 +14,16 @@ import java.util.List;
 
 public class ReverseArgsTest {
     private final String[] expectedData = {"1", "2", "3", "4"};
+    private final String[] actualData = {"1", "2", "3", "4"};
     private final List<String> expectedDataList = Arrays.asList(expectedData);
     private final String[] reverseExpectedData = {"4", "3", "2", "1"};
-    private final List<String> expectedEmptyData = new ArrayList<>(0);
-    private DataReceiverFactory dataReceiverFactory;
-    private PrinterFactory printerFactory;
+    private final List<String> expectedEmptyDataList = new ArrayList<>(0);
     private final static String EMPTY_CONSOLE_MESSAGE = "Console args is empty";
     private final static String EXPECTED_STRING_DATA = "1 2 3 4";
 
-    @Before
-    public void createFactories() {
-        dataReceiverFactory = new DataReceivingFactoryImpl();
-        printerFactory = new PrinterFactoryImpl();
-    }
-
     @Test
     public void shouldCorrectAddingDataUsingReceivingConsoleDataMethod() {
-        ConsoleReceiver consoleReceiver = dataReceiverFactory.getDataReceiver();
-        String[] actualData = {"1", "2", "3", "4"};
+        ConsoleReceiver consoleReceiver = new ConsoleDataReceiver();
         consoleReceiver.receivingConsoleData(actualData);
         List<String> actualDataList = consoleReceiver.getReceivingList();
         Assert.assertArrayEquals(expectedData, actualDataList.toArray());
@@ -50,7 +39,7 @@ public class ReverseArgsTest {
 
     @Test
     public void shouldPrintCorrectAnswerWithEmptyConsoleLine() {
-        Assert.assertEquals(EMPTY_CONSOLE_MESSAGE, getTestingConsoleOutput(expectedEmptyData));
+        Assert.assertEquals(EMPTY_CONSOLE_MESSAGE, getTestingConsoleOutput(expectedEmptyDataList));
     }
 
     @Test
@@ -60,13 +49,14 @@ public class ReverseArgsTest {
     }
 
     private String getTestingConsoleOutput(List<String> expectedData) {
-        Printer printer = printerFactory.getPrinter();
+        Printer printer = new ConsolePrinter();
         PrintStream consoleStream = System.out;
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream stream = new PrintStream(outputStream);
         System.setOut(stream);
         printer.printAnswer(expectedData);
         System.setOut(consoleStream);
-        return outputStream.toString().trim();
+        String resultString = outputStream.toString();
+        return resultString.trim();
     }
 }
