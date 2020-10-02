f07b8d1246ddfc29c29844c0a67a015f489e7dcd
Timur Radko <timurradko@gmail.com>
2020-10-02 13:38:56 +0300

    add some tiny changes

--- ArrayWrapper/src/main/java/com/epam/array/wrapper/data/reader/type/ConsoleDataReader.java
+++ ArrayWrapper/src/main/java/com/epam/array/wrapper/data/reader/type/ConsoleDataReader.java
@@ -9,13 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConsoleDataReader implements DataReadable {
+    private static final String KEYWORD = "exit";
 
     public int[] read() throws DataException {
         String stringNumber;
         List<Integer> filledList = new ArrayList<>();
         System.out.println("Please, enter the number (in the end enter keyword \"exit\"):");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            while (!(stringNumber = reader.readLine()).toLowerCase().equals("exit")) {
+            while (!(stringNumber = reader.readLine()).equalsIgnoreCase(KEYWORD)) {
                 int number = Integer.parseInt(stringNumber);
                 filledList.add(number);
             }
